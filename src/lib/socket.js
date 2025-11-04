1d1479ed37c3704f13edc8d02d161c075c6aba02
Parth Khambhadiya <pk.hmrtech@gmail.com>
2025-11-04 10:37:18 +0530

    added code for notification

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,11 +1,12 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "https://battle-zone-backend.onrender.com";
+// ✅ Read from .env file
+const SOCKET_URL = process.env.SOCKET_URL;
 
 export const socket = io(SOCKET_URL, {
-  autoConnect: false, // connect manually
-  transports: ["websocket"], // better stability
-  reconnection: true, // 🔁 auto reconnect
+  autoConnect: false,          // connect manually
+  transports: ["websocket"],   // better stability
+  reconnection: true,          // 🔁 auto reconnect
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
 });
