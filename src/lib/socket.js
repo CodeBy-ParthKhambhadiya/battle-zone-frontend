a54916434bfcf62aa6b57131c297706a5af0d0af
Parth Khambhadiya <pk.hmrtech@gmail.com>
2025-11-04 10:41:20 +0530

    added code for soket url

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -2,6 +2,7 @@ import { io } from "socket.io-client";
 
 // ✅ Read from .env file
 const SOCKET_URL = process.env.SOCKET_URL;
+console.log(SOCKET_URL);
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,          // connect manually
